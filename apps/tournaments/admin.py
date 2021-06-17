ad66d5180c43cea724b57eab7180a4a5be8b97c9
aybogdanov <aybogdanov@yandex-team.ru>
2021-06-17 13:35:55 +0300

    upd admin

--- apps/tournaments/admin.py
+++ apps/tournaments/admin.py
@@ -162,5 +162,5 @@ class ForecastAdmin(admin.ModelAdmin):
     ]
     list_display = ('display_title',)
     list_display_links = ('display_title',)
-    list_filter = ('tournament__title', 'user__username',)
+    list_filter = ('tournament__title', 'user__username', 'match__display_title', 'match_status',)
     ordering = ('match__start_date',)
